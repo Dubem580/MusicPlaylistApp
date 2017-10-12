1daeea7b7d15ff1fe79e8af9f10e192fff3c17a0
Dubem <dubemokam@gmail.com>
2017-10-12 14:07:38 -0700

    added methods to findSong(), and addedPlaylist()...

--- src/com/lesson/Song.java
+++ src/com/lesson/Song.java
@@ -2,5 +2,20 @@ package com.lesson;
 
 public class Song {
 
-    private
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
 }
